ecb078ba2ac6188e09fb76139964a252d11d8cf8
Ben <ben.crommen@gmail.com>
2021-04-03 18:24:05 +0200

    MaJ gitignore

---
+++ web/sites/default/files/php/twig/60624041eecca_recipe-duration.html.twig_Xvvif6nveVMUOU0R1UDHt9mNK/ZldqL_QbdNVTHHsHmRwPtsl3HlphdXBQaV9IAXejcpk.php
@@ -0,0 +1,113 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/recipe/templates/recipe-duration.html.twig */
+class __TwigTemplate_0600c1a8233d33d7f262a8ab31cea1f40ee2dfe26cfa7a7f89eb9f23854fa506 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 13
+        echo "
+";
+        // line 14
+        if ((($context["hours"] ?? null) && ($context["minutes"] ?? null))) {
+            // line 15
+            echo "  ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["hours"] ?? null), 15, $this->source), "html", null, true);
+            echo ", ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["minutes"] ?? null), 15, $this->source), "html", null, true);
+            echo "
+";
+        } elseif (        // line 16
+($context["hours"] ?? null)) {
+            // line 17
+            echo "  ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["hours"] ?? null), 17, $this->source), "html", null, true);
+            echo "
+";
+        } else {
+            // line 19
+            echo "  ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["minutes"] ?? null), 19, $this->source), "html", null, true);
+            echo "
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/recipe/templates/recipe-duration.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  59 => 19,  53 => 17,  51 => 16,  44 => 15,  42 => 14,  39 => 13,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/recipe/templates/recipe-duration.html.twig", "/Applications/MAMP/htdocs/blog_food/web/modules/contrib/recipe/templates/recipe-duration.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 14);
+        static $filters = array("escape" => 15);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/60624041eecca_recipe-total-time.html.tw_3csQtaeJ55wgrU-6p8PckGdUY/-GiNBB9pNt5ciDKU4ZpwLXYKAofp4XHuLcmkvfGEbco.php
@@ -0,0 +1,125 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/recipe/templates/recipe-total-time.html.twig */
+class __TwigTemplate_cf2d80988cb11117552742531671894fb6519e20b168eb50d77551bd616c34b2 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 19
+        $context["classes"] = [0 => "field", 1 => "field--name-recipe-total-time", 2 => "field--type-integer", 3 => ("field--label-" . $this->sandbox->ensureToStringAllowed(        // line 23
+($context["label_display"] ?? null), 23, $this->source))];
+        // line 27
+        $context["title_classes"] = [0 => "field__label", 1 => (((        // line 29
+($context["label_display"] ?? null) == "visually_hidden")) ? ("visually-hidden") : (""))];
+        // line 32
+        echo "
+";
+        // line 33
+        if (($context["label_hidden"] ?? null)) {
+            // line 34
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null), 1 => "field__item"], "method", false, false, true, 34), 34, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["total_time"] ?? null), 34, $this->source), "html", null, true);
+            echo "</div>
+";
+        } else {
+            // line 36
+            echo "  <div";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 36), 36, $this->source), "html", null, true);
+            echo ">
+    <div";
+            // line 37
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", [0 => ($context["title_classes"] ?? null)], "method", false, false, true, 37), 37, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 37, $this->source), "html", null, true);
+            echo "</div>
+    <div";
+            // line 38
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", [0 => "field__item"], "method", false, false, true, 38), 38, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["total_time"] ?? null), 38, $this->source), "html", null, true);
+            echo "</div>
+  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/recipe/templates/recipe-total-time.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  69 => 38,  63 => 37,  58 => 36,  50 => 34,  48 => 33,  45 => 32,  43 => 29,  42 => 27,  40 => 23,  39 => 19,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/recipe/templates/recipe-total-time.html.twig", "/Applications/MAMP/htdocs/blog_food/web/modules/contrib/recipe/templates/recipe-total-time.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 19, "if" => 33);
+        static $filters = array("escape" => 34);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
